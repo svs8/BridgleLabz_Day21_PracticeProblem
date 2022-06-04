e0f4c3efe2c204ca20cea7153b44201e1e301e24
svs8 <surakshithvittalshetty@gmail.com>
2022-06-04 13:19:39 +0530

    Added:Logic to check Last name

--- src/main/java/com/bridgelabz/UserValidation.java
+++ src/main/java/com/bridgelabz/UserValidation.java
@@ -4,7 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserValidation {
-    static void userFirstName() {
+
+    public static void userFirstName() {
 
         String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
         Scanner scanner = new Scanner(System.in);
@@ -21,9 +22,30 @@ public class UserValidation {
             System.out.println("First Name is Invalid");
     }
 
+    public static void userLastName() {
+
+        String lastNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.next();
+
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean result = matcher.matches();
+
+        if(result == true)
+            System.out.println("Last Name is Valid");
+        else
+            System.out.println("Last Name is Invalid");
+    }
+
+
+
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration program");
         userFirstName();
+        userLastName();
+
     }
 }
-
