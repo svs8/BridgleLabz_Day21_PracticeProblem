8b005b01bfff95a4f9dc6c570d96f30654a4b31d
svs8 <surakshithvittalshetty@gmail.com>
2022-06-04 13:15:54 +0530

    Added:Logic to check first name

--- src/main/java/com/bridgelabz/UserValidation.java
+++ src/main/java/com/bridgelabz/UserValidation.java
@@ -1,2 +1,29 @@
-package com.bridgelabz;public class UserValidation {
+package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidation {
+    static void userFirstName() {
+
+        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First Name:");
+        String firstName = scanner.next();
+
+        Pattern pattern = Pattern.compile(firstNamePattern);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean result = matcher.matches();
+
+        if(result == true)
+            System.out.println("First Name is Valid");
+        else
+            System.out.println("First Name is Invalid");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration program");
+        userFirstName();
+    }
 }
+
